498f7cbcb1d31593d623da251558f2a9ddf44bba
mtrop <mtropiano@gmail.com>
2020-04-12 16:24:05 -0400

    Updated pages.

---
+++ js/site.js
@@ -0,0 +1,26 @@
+var BR_PROJECTS = [
+	'JSON', 
+	'SQL', 
+	'Redis', 
+	'RookScript', 
+	'ArcheText', 
+	'Expression', 
+	'FileSystem'
+];
+
+var PROJECTS_ELEMENT = $Q1('.site-projects');
+
+$Q1('body').onload = function()
+{
+	$E(BR_PROJECTS,(e)=>{
+		PROJECTS_ELEMENT.appendChild(
+			$Element('a', {"href": e}, [
+				$Element('div', {"class": 'w3-button w3-center'}, [
+					$Element('img', {"class": 'site-project-logo', "src": e + '/logo.svg'}), 
+					$Text(e)
+				])
+			])
+		);
+	});
+	$IncludeHTML();
+};
